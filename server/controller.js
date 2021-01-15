64c7949bd4420fdeb7d64b0a6f06cc025d26492e
Ben Malamud-Roam <bmalamudroam@gmail.com>
2021-01-15 10:22:44 -0800

    added more specific status codes for api requests

--- server/controller.js
+++ server/controller.js
@@ -8,7 +8,7 @@ module.exports = {
         res.status(200).send(result);
       })
       .catch((err) => {
-        console.log('Error when getting home info from database: ', err);
+        res.status(403).send(err);
       });
   },
 
@@ -16,21 +16,20 @@ module.exports = {
   getAllHomes: (req, res) => {
     dbModel.getAllHomes()
       .then((homes) => {
-        console.log('homes: ', homes.length);
         res.status(200).send(homes);
       })
       .catch((err) => {
-        console.log('Error when getting all homes from database: ', err);
+        res.status(403).send(err);
       });
   },
 
   deleteAll: (req, res) => {
     dbModel.deleteAll()
       .then(() => {
-        res.status(200).send('Successfully cleared db');
+        res.status(204);
       })
       .catch((err) => {
-        console.log('Error when deleting all homes from database: ', err);
+        res.status(403).send(err);
       });
   },
 };
