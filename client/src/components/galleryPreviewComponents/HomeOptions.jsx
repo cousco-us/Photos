56eab7810e676da7e69965c8c6141478fb69c25f
Ben Malamud-Roam <bmalamudroam@gmail.com>
2021-01-17 18:06:37 -0800

    build HomeOptions component

---
+++ client/src/components/galleryPreviewComponents/HomeOptions.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const HomeDetails = ({ saved }) => {
+  let hasBeenSavedClass = 'not-saved';
+  if (saved) {
+    hasBeenSavedClass = 'saved';
+  }
+  return (
+    <div>
+      <h1>HomeDetails</h1>
+      <span>
+        <button type="submit" className="save-btn">
+          <div className={hasBeenSavedClass}>
+            {/* heart symbol */}
+            {'<3'}
+          </div>
+          Save
+        </button>
+        <button type="submit" className="share-btn">
+          {/* share symbol */}
+          [|] Save
+        </button>
+      </span>
+    </div>
+  );
+};
+
+export default HomeDetails;
