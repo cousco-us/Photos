e33e708a8bb19df42e54ed115da101db9da1a35b
Ben Malamud-Roam <bmalamudroam@gmail.com>
2021-01-26 11:50:43 -0800

    serving at port 3002

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const controller = require('./controller.js');
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 app.use(cors());
 app.use(express.json());
