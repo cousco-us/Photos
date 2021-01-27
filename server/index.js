17124f8f30090cf88c96c4cdcb71285094edbbb2
Ben Malamud-Roam <bmalamudroam@gmail.com>
2021-01-26 17:07:15 -0800

    add route for bundle

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 // serving static files not working
 app.use('/', express.static('public'));
+app.use('/bundle', express.static('public/dist/bundle.js'));
 
 app.get('/api/photoGallery/:homeId', controller.getHomeInfo);
 app.patch('/api/photoGallery/:homeId', controller.toggleHomeSaved);
